e9880a263887f553306739f786bb9883fc6656b7
klopezroX <klopezro@my.centennialcollege.ca>
2018-02-04 15:43:42 -0500

    Lab2Commit

---
+++ New Unity Project/Assets/Scripts/ClothesChangingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesChangingScript : MonoBehaviour {
+
+    public Sprite defaultSprite;
+    public Sprite redSprite;
+    public Sprite stylishSprite;
+    public SpriteRenderer defaultRenderer;
+    public GameObject buttonClicked;
+
+    void OnMouseDown()
+    {
+        if(buttonClicked.name == "DiamondButtonDefault")
+        {
+            defaultRenderer.sprite = defaultSprite;
+        }
+        else if (buttonClicked.name == "DiamondButtonRed")
+        {
+            defaultRenderer.sprite = redSprite;
+        }
+        else if (buttonClicked.name == "DiamondButtonStylish")
+        {
+            defaultRenderer.sprite = stylishSprite;
+        }
+    }
+}
